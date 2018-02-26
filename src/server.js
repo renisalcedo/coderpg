d19fe30faba20de636efdbe2dad6cb44a9e48b70
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-02-26 15:46:32 -0500

    Removed unneeded dependencies and added server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+// Controllers
+const User = require('./user/userController')
+const Role = require('./roles/roleController')
+
+const app = express()
+
+// Routes
+
+module.exports = app
